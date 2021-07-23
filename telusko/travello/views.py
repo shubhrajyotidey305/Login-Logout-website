2c34d45d738f94faac90e694603ba0b8a9438f2d
shubhrajyotidey305 <shubhrajyotidey305@gmail.com>
2021-07-23 12:03:52 +0530

    first commit

---
+++ telusko/travello/views.py
@@ -0,0 +1,8 @@
+from .models import Destination
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    dests = Destination.objects.all()    
+    return render(request, 'index.html', {'dests':dests})
